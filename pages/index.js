47a3de74ddd5552f54856db59f045d0a7c2f4a3f
Gbolaga Famodun <gbolaga@dotfusion.com>
2022-05-06 14:16:35 -0400

    skeleton and redirect

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Recipes({ recipes }) {
         .recipe-list {
           display: grid;
           grid-template-columns: 1fr 1fr;
-          grid-gap: 20px
+          grid-gap: 20px 60px;
         }
       `}</style>
     </div>
